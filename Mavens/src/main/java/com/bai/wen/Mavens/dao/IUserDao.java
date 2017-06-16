95422c1ce52a6b17a85a2c9b3234f82c1d9b23e7
Administrator <Administrator@MS-20170217WIWI>
2017-06-16 13:39:35 +0800

    成功

--- Mavens/src/main/java/com/bai/wen/Mavens/dao/IUserDao.java
+++
@@ -1,7 +0,0 @@
-package com.bai.wen.Mavens.dao;
-
-import com.bai.wen.Mavens.bean.User;
-
-public interface IUserDao {
-	User queryUser(User user);
-}
--- Mavens/src/main/java/com/bai/wen/Mavens/service/IUserService.java
+++
@@ -1,7 +0,0 @@
-package com.bai.wen.Mavens.service;
-
-import com.bai.wen.Mavens.bean.User;
-
-public interface IUserService {
-	User queryUser(User user);
-}
