2bd02c532129fcae3c2399ac95f370105684c2b3
Administrator <Administrator@MS-20170217WIWI>
2017-06-16 12:30:55 +0800

    删除

--- planeticket/src/main/java/dao/IManagerDao.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import bean.Manager;
-
-public interface IManagerDao {
-	Manager querymanager(Manager Manager);
-
-}
--- planeticket/src/main/java/service/IManagerService.java
+++
@@ -1,6 +0,0 @@
-package service;
-import bean.Manager;
-
-public interface IManagerService {
-	Manager queryManager(Manager Manager);
-}
